290c028526005439f14037575025d8ad65ce12e6
FaariD01 <moohannafarid@gmail.com>
2025-10-12 20:43:23 -0300

    Punto 6 realizado

--- Unidad_Funciones/TP2_Funciones.py
+++ Unidad_Funciones/TP2_Funciones.py
@@ -85,3 +85,26 @@ def segundos_a_horas(segundos):
 print(segundos_a_horas(3600))
 
 """
+
+"""
+
+Crear una función llamada tabla_multiplicar(numero) que reciba un
+número como parámetro y imprima la tabla de multiplicar de ese
+número del 1 al 10. Pedir al usuario el número y llamar a la función.
+
+
+def tabla_multiplicar(numero):
+    
+    print(f"Tabla de multiplicar del numero {numero}")
+    
+    
+    for i in range(1,11):
+        
+        print(f"{numero} x {i}: {numero*i}")
+        
+
+numero = int(input("Ingrese un numero:  "))
+tabla_multiplicar(numero)
+
+
+"""
