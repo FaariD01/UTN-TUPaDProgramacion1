ad1627e954725de517187c9a77177800e314fe4c
FaariD01 <moohannafarid@gmail.com>
2025-10-12 20:46:35 -0300

    Punto 7 realizado

--- Unidad_Funciones/TP2_Funciones.py
+++ Unidad_Funciones/TP2_Funciones.py
@@ -108,3 +108,31 @@ tabla_multiplicar(numero)
 
 
 """
+
+"""
+
+7. Crear una función llamada operaciones_basicas(a, b) que reciba
+dos números como parámetros y devuelva una tupla con el resultado de sumarlos, restarlos,
+multiplicarlos y dividirlos. Mostrar los resultados de forma clara.
+
+
+def operaciones_basicas(a, b):
+    suma = a + b
+    resta = a - b
+    multiplicacion = a * b
+    division = a / b
+    return (suma, resta, multiplicacion, division)
+
+
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+
+resultados = operaciones_basicas(num1, num2)
+
+print(f"Suma: {resultados[0]}")
+print(f"Resta: {resultados[1]}")
+print(f"Multiplicación: {resultados[2]}")
+print(f"División: {resultados[3]}")
+
+"""
+
