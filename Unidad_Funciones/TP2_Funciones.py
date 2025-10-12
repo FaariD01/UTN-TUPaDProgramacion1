e139975dd8523eed6fe6bee0e36168c57e22b13b
FaariD01 <moohannafarid@gmail.com>
2025-10-12 20:35:36 -0300

    Punto 5 realizado

--- Unidad_Funciones/TP2_Funciones.py
+++ Unidad_Funciones/TP2_Funciones.py
@@ -70,3 +70,18 @@ print(f"Perimetro: {calcular_perimetro_circulo(radio)}")
 
 
 """
+
+
+"""
+
+Crear una función llamada segundos_a_horas(segundos) que reciba
+una cantidad de segundos como parámetro y devuelva la cantidad
+de horas correspondientes. Solicitar al usuario los segundos y mostrar el resultado usando esta función.
+
+def segundos_a_horas(segundos):
+    
+    return segundos / 3600
+
+print(segundos_a_horas(3600))
+
+"""
