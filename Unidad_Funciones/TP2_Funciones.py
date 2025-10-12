45a0aef1e667405dfdfdf581bc1ff9d61cadad5f
FaariD01 <moohannafarid@gmail.com>
2025-10-12 20:25:32 -0300

    Realizados las 2 primeras consignas

---
+++ Unidad_Funciones/TP2_Funciones.py
@@ -0,0 +1,34 @@
+"""
+
+1. Crear una función llamada imprimir_hola_mundo que imprima por
+pantalla el mensaje: “Hola Mundo!”. Llamar a esta función desde el
+programa principal.
+
+
+def imprimir_hola_mundo():
+    
+    print("Hola Mundo")
+    
+imprimir_hola_mundo()
+
+"""
+
+"""
+
+2. Crear una función llamada saludar_usuario(nombre) que reciba
+como parámetro un nombre y devuelva un saludo personalizado.
+Por ejemplo, si se llama con saludar_usuario("Marcos"), deberá devolver: “Hola Marcos!”. Llamar a esta función desde el programa
+principal solicitando el nombre al usuario.
+
+
+
+def saludar_usuario(nombre):
+    
+    
+    
+    print(f"Hola {nombre} !!")
+    
+saludar_usuario("Juan")   
+
+"""
+
