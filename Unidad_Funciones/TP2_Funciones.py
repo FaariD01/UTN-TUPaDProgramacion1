be867f00c193c548a492dc077a976b03e4f88d93
FaariD01 <moohannafarid@gmail.com>
2025-10-12 20:33:18 -0300

    Punto 4 realizado

--- Unidad_Funciones/TP2_Funciones.py
+++ Unidad_Funciones/TP2_Funciones.py
@@ -48,3 +48,25 @@ def informacion_personal(nombre, apellido, edad, residencia):
 informacion_personal("Juan","Martinez",30,"Buenos Aires")
 
 """
+
+"""
+
+4. Crear dos funciones: calcular_area_circulo(radio) que reciba el radio como parámetro
+y devuelva el área del círculo. calcular_perimetro_circulo(radio) que reciba el radio como parámetro
+y devuelva el perímetro del círculo. Solicitar el radio al usuario y llamar ambas funciones para mostrar los resultados.
+
+
+
+def calcular_area_circulo(radio):
+    return 3.14 * (radio**2)
+
+def calcular_perimetro_circulo(radio):
+    return 2 * 3.14 * radio
+
+radio = int(input("Ingrese el radio..."))
+
+print(f"Radio: {calcular_area_circulo(radio)}")
+print(f"Perimetro: {calcular_perimetro_circulo(radio)}")
+
+
+"""
