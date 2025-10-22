0a46c507ef2719fad7ad68765687458667114c81
FaariD01 <moohannafarid@gmail.com>
2025-10-22 20:24:07 -0300

    Realice punto 3

--- Unidad_7_Datos_Complejos/TP6_Estructura_De_Datos_Complejos.py
+++ Unidad_7_Datos_Complejos/TP6_Estructura_De_Datos_Complejos.py
@@ -35,3 +35,15 @@ precios_frutas['Manzana'] = 1700
 precios_frutas['Melón'] = 2800
 
 print(precios_frutas)
+
+"""
+
+3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código 
+desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los 
+precios. 
+
+"""
+
+lista_frutas = list(precios_frutas.keys())
+
+print(lista_frutas)
