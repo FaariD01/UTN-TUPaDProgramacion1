96de9a28350ee6057579731c70a338a197f7f917
FaariD01 <moohannafarid@gmail.com>
2025-11-06 21:31:17 -0300

    Realice el punto 4

--- Unidad_8_Manejo_de_Archivos/TP7_Archivos.py
+++ Unidad_8_Manejo_de_Archivos/TP7_Archivos.py
@@ -12,6 +12,7 @@ Actividades:
 1. Crear archivo inicial con productos: Crear un archivo de texto llamado 
 productos.txt con tres productos. Cada línea debe tener:  nombre,precio,cantidad
 """
+"""
 
 with open("Unidad_8_Manejo_de_Archivos/productos.txt", "w") as archivo:
     archivo.write("Shampoo,1500,20\n")
@@ -20,11 +21,11 @@ with open("Unidad_8_Manejo_de_Archivos/productos.txt", "w") as archivo:
 
 print(f"Archivo creado")
 
-"""
+
 2. Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada 
 línea, la procese con .strip() y .split(","), y muestre los productos en el siguiente 
 formato: 
-"""
+
 
 with open("Unidad_8_Manejo_de_Archivos/productos.txt", "r") as archivo:
     print("Listado de productos:\n")
@@ -35,11 +36,11 @@ with open("Unidad_8_Manejo_de_Archivos/productos.txt", "r") as archivo:
             
         print(f"Producto: {nombre} | Precio: ${precio} | Cantidad: {cantidad}")
 
-"""
+
 3. Agregar productos desde teclado: Modificar el programa para que luego de mostrar 
 los productos, le pida al usuario que ingrese un nuevo producto (nombre, precio, 
 cantidad) y lo agregue al archivo sin borrar el contenido existente.
-"""
+
 
 
 with open("Unidad_8_Manejo_de_Archivos/productos.txt", "r") as archivo:
@@ -59,3 +60,42 @@ with open("Unidad_8_Manejo_de_Archivos/productos.txt", "a") as archivo:
     archivo.write(f"\n{nombre_nuevo},{precio_nuevo},{cantidad_nueva}")
 
 print("\nProducto agregado correctamente al archivo.")
+
+"""
+
+
+"""
+4. Cargar productos en una lista de diccionarios: Al leer el archivo, cargar los datos en 
+una lista llamada productos, donde cada elemento sea un diccionario con claves: 
+nombre, precio, cantidad.
+"""
+
+productos = []
+
+with open("Unidad_8_Manejo_de_Archivos/productos.txt", "r") as archivo:
+    for linea in archivo:
+        linea = linea.strip()
+        if not linea:
+            continue  # salta líneas vacías
+        partes = linea.split(",")
+        if len(partes) != 3:
+            print(f"Línea inválida encontrada y omitida: {linea}")
+            continue
+        nombre, precio, cantidad = partes
+
+        producto = {
+            "nombre": nombre,
+            "precio": float(precio),
+            "cantidad": int(cantidad)
+        }
+        productos.append(producto)
+
+print("Listado de productos cargados en memoria:\n")
+for p in productos:
+    print(f"Producto: {p['nombre']} | Precio: ${p['precio']} | Cantidad: {p['cantidad']}")
+
+"""
+5. Buscar producto por nombre: Pedir al usuario que ingrese el nombre de un 
+producto. Recorrer la lista de productos y, si lo encuentra, mostrar todos sus datos. Si 
+no existe, mostrar un mensaje de error. 
+"""
