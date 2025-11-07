25afcc2060ccb983a73d6992aaf3e6b03b32725a
FaariD01 <moohannafarid@gmail.com>
2025-11-06 21:21:12 -0300

    Ajuste el print del punto 2

--- Unidad_8_Manejo_de_Archivos/TP7_Archivos.py
+++ Unidad_8_Manejo_de_Archivos/TP7_Archivos.py
@@ -33,6 +33,4 @@ with open("Unidad_8_Manejo_de_Archivos/productos.txt", "r") as archivo:
         linea = linea.strip()
         nombre, precio, cantidad = linea.split(",")
             
-        print(f"Nombre: {nombre}")
-        print(f"Precio: ${precio}")
-        print(f"Cantidad: {cantidad} unidades\n")
+        print(f"Producto: {nombre} | Precio: ${precio} | Cantidad: {cantidad}")
