fc049fa3f2705af172c8656e4a776e1fcb3c7ecb
FaariD01 <moohannafarid@gmail.com>
2025-11-06 22:13:39 -0300

    Realice el punto 3

--- Unidad_9_Recursividad/TP11_Recursividad.py
+++ Unidad_9_Recursividad/TP11_Recursividad.py
@@ -51,3 +51,22 @@ pos = int(input("Ingrese la posición hasta donde mostrar la serie de Fibonacci:
 print("Serie de Fibonacci:")
 for i in range(pos):
     print(fibonacci(i), end=" ")
+
+"""
+3) Crea una función recursiva que calcule la potencia de un número base elevado a un 
+exponente, utilizando la fórmula 𝑛𝑚 = 𝑛 ∗ 𝑛(𝑚−1). Prueba esta función en un 
+algoritmo general. 
+"""
+
+def potencia(base, exponente):
+    if exponente == 0:     # Caso base
+        return 1
+    else:                  # Caso recursivo
+        return base * potencia(base, exponente - 1)
+
+
+# Programa principal
+b = int(input("Ingrese la base: "))
+e = int(input("Ingrese el exponente: "))
+
+print(f"{b}^{e} = {potencia(b, e)}")
