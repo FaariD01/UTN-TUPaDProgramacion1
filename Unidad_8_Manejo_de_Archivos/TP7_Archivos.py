279df302d92c366a55c35ea2ead88b6dd11e43f2
FaariD01 <moohannafarid@gmail.com>
2025-11-06 21:20:28 -0300

    Realice el punto 2 y ajuste el 1

--- Unidad_8_Manejo_de_Archivos/TP7_Archivos.py
+++ Unidad_8_Manejo_de_Archivos/TP7_Archivos.py
@@ -12,15 +12,27 @@ Actividades:
 1. Crear archivo inicial con productos: Crear un archivo de texto llamado 
 productos.txt con tres productos. Cada línea debe tener:  nombre,precio,cantidad
 """
-import os
 
-carpeta = "Unidad_8_Manejo_de_Archivos"
-
-ruta_archivo = os.path.join(carpeta, "productos.txt")
-
-with open(ruta_archivo, "w") as archivo:
+with open("Unidad_8_Manejo_de_Archivos/productos.txt", "w") as archivo:
     archivo.write("Shampoo,1500,20\n")
     archivo.write("Acondicionador,1800,15\n")
     archivo.write("Gel,1200,10\n")
 
-print(f"Archivo creado en: {os.path.abspath(ruta_archivo)}")
+print(f"Archivo creado")
+
+"""
+2. Leer y mostrar productos: Crear un programa que abra productos.txt, lea cada 
+línea, la procese con .strip() y .split(","), y muestre los productos en el siguiente 
+formato: 
+"""
+
+with open("Unidad_8_Manejo_de_Archivos/productos.txt", "r") as archivo:
+    print("Listado de productos:\n")
+    
+    for linea in archivo:
+        linea = linea.strip()
+        nombre, precio, cantidad = linea.split(",")
+            
+        print(f"Nombre: {nombre}")
+        print(f"Precio: ${precio}")
+        print(f"Cantidad: {cantidad} unidades\n")
