0464debf196be4b3d569c20f4ffaaf9aa5487c2b
FaariD01 <moohannafarid@gmail.com>
2025-11-06 22:14:53 -0300

    Realice el punto 8

--- Unidad_9_Recursividad/TP11_Recursividad.py
+++ Unidad_9_Recursividad/TP11_Recursividad.py
@@ -70,3 +70,124 @@ b = int(input("Ingrese la base: "))
 e = int(input("Ingrese el exponente: "))
 
 print(f"{b}^{e} = {potencia(b, e)}")
+
+"""
+4) Crear una función recursiva en Python que reciba un número entero positivo en base 
+decimal y devuelva su representación en binario como una cadena de texto. 
+
+"""
+
+def decimal_a_binario(n):
+    if n == 0:             # Caso base
+        return ""
+    else:                  # Caso recursivo
+        return decimal_a_binario(n // 2) + str(n % 2)
+
+
+# Programa principal
+num = int(input("Ingrese un número entero positivo: "))
+
+# Si el número es 0, el resultado debe ser "0"
+resultado = decimal_a_binario(num)
+if resultado == "":
+    resultado = "0"
+
+print(f"El número {num} en binario es: {resultado}")
+
+"""
+5) Implementá una función recursiva llamada es_palindromo(palabra) que reciba una 
+cadena de texto sin espacios ni tildes, y devuelva True si es un palíndromo o False si no 
+lo es. 
+     Requisitos: 
+La solución debe ser recursiva. 
+No se debe usar [::-1] ni la función reversed().
+"""
+def es_palindromo(palabra):
+    if len(palabra) <= 1:       # Caso base
+        return True
+    elif palabra[0] != palabra[-1]:
+        return False
+    else:                       # Caso recursivo
+        return es_palindromo(palabra[1:-1])
+
+
+# Programa principal
+texto = input("Ingrese una palabra: ").lower()
+if es_palindromo(texto):
+    print("Es un palíndromo ")
+else:
+    print("No es un palíndromo ")
+
+"""
+6) Escribí una función recursiva en Python llamada suma_digitos(n) que reciba un 
+número entero positivo y devuelva la suma de todos sus dígitos. 
+     Restricciones: 
+No se puede convertir el número a string. 
+Usá operaciones matemáticas (%, //) y recursión. 
+Ejemplos: 
+suma_digitos(1234)   → 10  (1 + 2 + 3 + 4) 
+suma_digitos(9)      → 9 
+suma_digitos(305)    → 8   (3 + 0 + 5)
+"""
+
+def suma_digitos(n):
+    if n < 10:              # Caso base
+        return n
+    else:                   # Caso recursivo
+        return (n % 10) + suma_digitos(n // 10)
+
+
+# Programa principal
+num = int(input("Ingrese un número entero positivo: "))
+print(f"La suma de los dígitos de {num} es: {suma_digitos(num)}")
+
+"""
+7) Un niño está construyendo una pirámide con bloques. En el nivel más bajo coloca n 
+bloques, en el siguiente nivel uno menos (n - 1), y así sucesivamente hasta llegar al 
+último nivel con un solo bloque. 
+ 
+Escribí una función recursiva contar_bloques(n) que reciba el número de bloques en el 
+nivel más bajo y devuelva el total de bloques que necesita para construir toda la 
+pirámide. 
+ 
+      Ejemplos: 
+contar_bloques(1)   → 1         (1) 
+contar_bloques(2)   → 3         (2 + 1) 
+contar_bloques(4)   → 10        (4 + 3 + 2 + 1)
+"""
+
+def contar_bloques(n):
+    if n == 1:              # Caso base
+        return 1
+    else:                   # Caso recursivo
+        return n + contar_bloques(n - 1)
+
+
+# Programa principal
+niveles = int(input("Ingrese la cantidad de bloques del nivel más bajo: "))
+print(f"Total de bloques necesarios: {contar_bloques(niveles)}")
+
+
+"""
+8) Escribí una función recursiva llamada contar_digito(numero, digito) que reciba un 
+número entero positivo (numero) y un dígito (entre 0 y 9), y devuelva cuántas veces 
+aparece ese dígito dentro del número. 
+      Ejemplos: 
+contar_digito(12233421, 2)   → 3   
+contar_digito(5555, 5)       → 4
+"""
+
+def contar_digito(numero, digito):
+    if numero == 0:         # Caso base
+        return 0
+    elif numero % 10 == digito:
+        return 1 + contar_digito(numero // 10, digito)
+    else:
+        return contar_digito(numero // 10, digito)
+
+
+# Programa principal
+num = int(input("Ingrese un número entero positivo: "))
+dig = int(input("Ingrese el dígito a contar (0-9): "))
+
+print(f"El dígito {dig} aparece {contar_digito(num, dig)} veces en {num}.")
